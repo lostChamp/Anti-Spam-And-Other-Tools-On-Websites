d2afaa4b86ed7aebffe233183955d30610f9faba
lostChamp <lost.champ2007@gmail.com>
2024-03-14 09:29:24 +0500

    You need this if you are dev websites

---
+++ utmMarks/utmMarks.php
@@ -0,0 +1,5 @@
+<input type="hidden" name="utm" value="">
+<input type="hidden" name="utm_campaign" value="<?= $_GET["utm_campaign"] ?? ''?>">
+<input type="hidden" name="utm_medium" value="<?= $_GET["utm_medium"] ?? ''?>">
+<input type="hidden" name="utm_content" value="<?= $_GET["utm_content"] ?? ''?>">
+<input type="hidden" name="utm_term" value="<?= $_GET["utm_term"] ?? ''?>">
